7e932199e77b24fa68c2c8d00c28c76f1df13b7a
EdmilsonColombini91 <edmilson.colombini@gmail.com>
2021-10-17 20:51:23 -0300

    Fazendo o carrinho de compras

--- app/src/main/java/com/colombini/blprodutosapp/activities/ActivityVenda.java
+++ app/src/main/java/com/colombini/blprodutosapp/activities/ActivityVenda.java
@@ -3,14 +3,75 @@ package com.colombini.blprodutosapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.colombini.blprodutosapp.R;
+import com.colombini.blprodutosapp.controller.ProdutoCtrl;
+import com.colombini.blprodutosapp.dbHelper.ConexaoSQLite;
+import com.colombini.blprodutosapp.model.ItemDoCarrinho;
+import com.colombini.blprodutosapp.model.Produto;
+
+import java.util.List;
 
 public class ActivityVenda extends AppCompatActivity {
+
+    private Spinner spnProdutos;
+    private List<Produto> listaProduto;
+    private ProdutoCtrl produtoCtrl;
+    private EditText quantidadeItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venda);
+
+        //spinner
+        this.produtoCtrl = new ProdutoCtrl(ConexaoSQLite.getInstancia(this));
+        this.listaProduto = this.produtoCtrl.getListaProdutosCTrl();
+
+        this.spnProdutos = (Spinner)  this.findViewById(R.id.spnProduto);
+        ArrayAdapter<Produto> spnProdutoAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                this.listaProduto
+        );
+
+        this.spnProdutos.setAdapter(spnProdutoAdapter);
+
+        //end spinner
+
+        this
+                .quantidadeItem = (EditText) this.findViewById(R.id.edtQuantidadeProduto);
+
+    }
+
+    /**
+     * Clique no botão de add produtos ao carrinho
+     * @param view
+     */
+    public void eventAddProduto(View view){
+
+        ItemDoCarrinho itemDoCarrinho = new ItemDoCarrinho();
+        Produto produtoSelecionado = (Produto) this.spnProdutos.getSelectedItem();
+
+        int quantidadeProduto = 0;
+
+        if (this.quantidadeItem.getText().toString().equals("")) {
+            quantidadeProduto = 1;
+        } else {
+            quantidadeProduto = Integer.parseInt(this.quantidadeItem.getText().toString());
+        }
+
+        itemDoCarrinho.setNome(produtoSelecionado.getNome());
+        itemDoCarrinho.setQuantidadeSelecionada(quantidadeProduto);
+        itemDoCarrinho.setPrecoProduto(produtoSelecionado.getPreco());
+        itemDoCarrinho.setPrecoDoItemVenda(itemDoCarrinho.getPrecoProduto() * itemDoCarrinho.getQuantidadeSelecionada());
+
+
+
     }
 }
---
+++ app/src/main/java/com/colombini/blprodutosapp/adapters/AdapterItensDoCarrinho.java
@@ -0,0 +1,72 @@
+package com.colombini.blprodutosapp.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.colombini.blprodutosapp.R;
+import com.colombini.blprodutosapp.model.ItemDoCarrinho;
+import com.colombini.blprodutosapp.model.Produto;
+
+import java.util.List;
+
+/**
+ * Created by Edmilson 17/10/2021
+ */
+
+public class AdapterItensDoCarrinho extends BaseAdapter {
+
+    private Context context;
+    private List<ItemDoCarrinho> itensDoCarrinho;
+
+    public AdapterItensDoCarrinho(Context context, List<ItemDoCarrinho> itensDoCarrinho) {
+        this.context = context;
+        this.itensDoCarrinho = itensDoCarrinho;
+    }
+
+    @Override
+    public int getCount() {return this.itensDoCarrinho.size();}
+
+    @Override
+    public Object getItem(int posicao) {return this.itensDoCarrinho.get(posicao);}
+
+    @Override public long getItemId(int posicao) {return posicao;}
+
+    public void removerItemDoCarrinho (int posicao) {
+        this.itensDoCarrinho.remove(posicao);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public View getView(int posicao, View view, ViewGroup viewGroup) {
+
+        View v = View.inflate(this.context, R.layout.layout_produto, null);
+
+        TextView tvNomeProduto = (TextView)  v.findViewById(R.id.tvNomeProduto);
+        TextView tvPrecoProduto = (TextView)  v.findViewById(R.id.tvPrecoProduto);
+        TextView tvQuantidadeSelecionada = (TextView)  v.findViewById(R.id.tvEstoqueProduto);
+        TextView tvValorItem = (TextView) v.findViewById(R.id.tvValorTotalItem);
+
+        tvNomeProduto.setText(this.itensDoCarrinho.get(posicao).getNome());
+        tvPrecoProduto.setText(String.valueOf(this.itensDoCarrinho.get(posicao).getPrecoProduto()));
+        tvQuantidadeSelecionada.setText(String.valueOf(this.itensDoCarrinho.get(posicao).getQuantidadeSelecionada()));
+        tvValorItem.setText(String.valueOf(this.itensDoCarrinho.get(posicao).getPrecoDoItemVenda()));
+
+        return v;
+    }
+
+    /**
+     * Atualiza a lista de produtos do adapter
+     * @param pItensDoCarrinho
+     */
+
+    @SuppressWarnings("JavadocReference")
+    public void atualizar(List<ItemDoCarrinho> pItensDoCarrinho){
+        this.itensDoCarrinho.clear();
+        this.itensDoCarrinho = pItensDoCarrinho;
+        this.notifyDataSetChanged();
+    }
+
+}
